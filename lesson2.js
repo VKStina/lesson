2d8c6757945bfb7a2e94906b3273c9ceaf6402c1
VKStina <101672203+VKStina@users.noreply.github.com>
2022-04-04 17:30:11 +0200

    Add files via upload

---
+++ lesson2.js
@@ -0,0 +1,27 @@
+//lesson2
+
+
+//2
+let user = "ivan";
+let newUser = user.charAt(0).toUpperCase() + user.slice(1);
+console.log(newUser);
+
+//4
+let a = '';
+let b = 'bbb';
+let c = false;
+let test1 = !b.length===c;
+let test2 = !b.length&&!b.length;
+let test3 = a.length>b.length;
+let test4 = a===c;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(test1);
+console.log(test2);
+console.log(test3);
+console.log(test4);
+
+
+
+
